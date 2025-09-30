c9ec52a9319df34487f3d5ccea0bb6c5500bd82a
sayosigure <weekend_camp@outlook.jp>
2025-09-30 12:17:22 +0900

    Update 計算機.py

--- 計算機.py
+++ 計算機.py
@@ -1,7 +1,3 @@
-import streamlit as st
-
-st.title("🧮 シンプル電卓")
-
 number1=int(input("1つめ"))
 number2=int(input("2つめ"))
 hugou=input("四則演算")
@@ -15,8 +11,3 @@ elif hugou=="/":
         print(number1/number2)
 else:
     print("条件の見直し")
-    elif op == "/":
-        if b != 0:
-            st.write("結果:", a / b)
-        else:
-            st.write("⚠️ 0では割れません")
