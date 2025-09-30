339becbc4f17bb56e1525cb61797fe7167d584c7
sayosigure <weekend_camp@outlook.jp>
2025-09-30 12:12:56 +0900

    Update 計算機.py
    自分で１から制作

--- 計算機.py
+++ 計算機.py
@@ -2,21 +2,19 @@ import streamlit as st
 
 st.title("🧮 シンプル電卓")
 
-# 入力
-a = st.number_input("1つ目の数字を入力してください", value=0)
-b = st.number_input("2つ目の数字を入力してください", value=0)
-
-# 演算子を選択
-op = st.selectbox("演算子を選んでください", ["+", "-", "*", "/"])
-
-# 計算ボタン
-if st.button("計算する"):
-    if op == "+":
-        st.write("結果:", a + b)
-    elif op == "-":
-        st.write("結果:", a - b)
-    elif op == "*":
-        st.write("結果:", a * b)
+number1=int(input("1つめ"))
+number2=int(input("2つめ"))
+hugou=input("四則演算")
+if hugou=="+":
+    print(number1+number2)
+elif hugou=="-":
+        print(number1-number2)
+elif hugou=="*":
+        print(number1*number2)
+elif hugou=="/":
+        print(number1/number2)
+else:
+    print("条件の見直し")
     elif op == "/":
         if b != 0:
             st.write("結果:", a / b)
